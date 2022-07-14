580ae3b8acaaaf043d06fffb490aca5fc3c44569
Girish Tiwale <girishmt4@gmail.com>
2022-07-14 12:09:01 -0700

    added geocoding and maps to the show page

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,5 +1,9 @@
 const Campground = require('../models/campground');
 const { cloudinary } = require('../cloudinary');
+const mapboxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapboxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mapboxGeocoding({ accessToken: mapboxToken });
+
 
 module.exports.index = async (req, res) => {
     const campgrounds = await Campground.find({});
@@ -12,7 +16,12 @@ module.exports.renderNewForm = (req, res) => {
 
 module.exports.createCampground = async (req, res, next) => {
     const campground = req.body.campground;
+    const geoData = await geocoder.forwardGeocode({
+        query: campground.location,
+        limit: 1
+    }).send();
     const newCampground = new Campground(campground);
+    newCampground.geometry = geoData.body.features[0].geometry;
     newCampground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
     newCampground.author = req.user._id;
     await newCampground.save();
