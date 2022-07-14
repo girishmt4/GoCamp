3f503f0504cb7dd6ec7f89cfdb0d0c5c3c565d8c
Girish Tiwale <girishmt4@gmail.com>
2022-07-13 23:00:37 -0700

    added images delete functionality

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,5 @@
 const Campground = require('../models/campground');
+const { cloudinary } = require('../cloudinary');
 
 module.exports.index = async (req, res) => {
     const campgrounds = await Campground.find({});
@@ -48,8 +49,20 @@ module.exports.renderEditForm = async (req, res) => {
 
 module.exports.updateCampground = async (req, res) => {
     const { id } = req.params;
+    console.log(req.body);
     const newCampground = req.body.campground;
     const campground = await Campground.findByIdAndUpdate(id, newCampground);
+    const images = req.files.map(f => ({ url: f.path, filename: f.filename }));
+    campground.images.push(...images);
+    await campground.save();
+
+    if (req.body.deleteImages) {
+        for (let filename of req.body.deleteImages) {
+            await cloudinary.uploader.destroy(filename);
+        }
+        await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } });
+    }
+
     req.flash('success', 'Successfully Updated the campground');
     res.redirect(`/campgrounds/${campground.id}`);
 }
